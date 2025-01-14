60078a1671c2de471a085771463564e3321e5e91
AlexTheDevOnMicrobit <Alextout52@gmail.Com>
2025-01-14 16:30:24 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.py
@@ -0,0 +1,17 @@
+for Boot in range(101):
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        """)
+    basic.clear_screen()
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        """)
+    Boot += 1
---
+++ main.ts
@@ -0,0 +1,34 @@
+let Time = 0
+input.onButtonPressed(Button.A, function () {
+    basic.pause(500)
+    Time = 37
+    for (let index = 0; index < 37; index++) {
+        basic.showNumber(Time)
+        Time = Time - 1
+        basic.pause(1000)
+    }
+    music.setVolume(255)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    music.play(music.stringPlayable("C C C E D G E D ", 12), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.pause(500)
+    Time = 37
+    for (let index = 0; index < 37; index++) {
+        basic.showNumber(Time)
+        Time = Time - 1
+        basic.pause(1000)
+    }
+    music.setVolume(255)
+    basic.showIcon(IconNames.Yes)
+    music.play(music.stringPlayable("C F B C A E C C5 ", 323), music.PlaybackMode.UntilDone)
+})
+input.onGesture(Gesture.Shake, function () {
+    control.reset()
+})
